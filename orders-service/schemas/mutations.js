8440f3ce6977d71c92a0c932fa66adc53e163861
vic6595 <vic9565@hotmail.com>
2019-05-10 22:35:45 -0600

    stable services, correction on node mutation

--- orders-service/schemas/mutations.js
+++ orders-service/schemas/mutations.js
@@ -17,17 +17,16 @@ const RootMutation = new GraphQLObjectType({
         productId: { type: GraphQLString }
       },
       resolve(parentValue, args) {
-        // var legit = jwt.verify(args.token, KEY);
-        // console.log("\nJWT verification result: " + JSON.stringify(legit));
-        // var decoded =  jwt.decode(args.token, {complete: true});
-        // console.log("\nDecoded jwt: "+ JSON.stringify(decoded));
-        // let userFromToken = decoded.payload.data.userId;
+        var legit = jwt.verify(args.token, KEY);
+        console.log("\nJWT verification result: " + JSON.stringify(legit));
+        var decoded =  jwt.decode(args.token, {complete: true});
+        console.log("\nDecoded jwt: "+ JSON.stringify(decoded));
+        let userFromToken = decoded.payload.data.userId;
 
         
         const query = `INSERT INTO orders( userId, productId ) VALUES ($1,$2) RETURNING id, userId, productId, issueDate`;
         const values = [
-          // userFromToken,
-          args.token,
+          userFromToken,
           args.productId
         ];
         
